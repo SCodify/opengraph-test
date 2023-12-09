3123cb9539221f25dc8b92adaf2b4c683a23cc0f
Christian Ledesma <softcodify@gmail.com>
2023-12-09 01:07:38 -0300

    Se eliminó etiqueta OpenGraph

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      <OpenGraph enlace={"https://open-graph-test.netlify.app/"}/>
+      {/* <OpenGraph enlace={"https://open-graph-test.netlify.app/"}/> */}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
